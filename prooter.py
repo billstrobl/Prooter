3d284fc8ed515c399501c013c597711d2b33f5b2
bstrobl <bstrobl@zynga.com>
2021-01-12 12:45:17 -0600

    Added file output and readme. And code cleanup.

--- prooter.py
+++ prooter.py
@@ -1,56 +1,63 @@
-#############################################################
+#!/usr/bin/env python3
+########################################################################################################################
 #
-# ParlerRooter.py
+# PRooter.py
 #
-# Search through posts from Parler to find real and usernames
+# Searches through HTML posts to find names, usernames, and posts. Creates an output that can be used elsewhere.
 #
 # Notes: This script relies on having the data locally available.
 # Future versions may include allowing remote source urls.
 # Specify the correct path to your data in the path var on line 17.
-#############################################################
+########################################################################################################################
 
 import os
 import re
 import pprint as pp
-from pathlib import Path
 
-PATH = Path('./data')
-
+PATH = (str(os.path.dirname(os.path.realpath(__file__))) + '/data/')
+OUTPUT_PATH = (str(os.path.dirname(os.path.realpath(__file__))) + '/results/result.txt')
 
 def get_file_list():
     this_file_list = []
     for root, dirs, files in os.walk(PATH):
         for file in files:
             this_file_list.append(str(file))
-            print('Added ' + str(file) + 'to scan list.')
+            print('\nAdded {} to scan list.'.format(str(file)))
+    print('\nFile list complete!')
     return this_file_list
 
 
 def check_files(list_to_check):
-    print('\n\nScanning File List . . .\n\n')
-    data_list = []
+    print('\nStarting file scan . . .')
+    results_list = []
 
-    for i in list_to_check:
-        this_filename = i
-        pp.pprint("Checking " + this_filename + ". . .")
-        with open('./data/' + this_filename, encoding="utf8") as this_file:
+    for this_filename in list_to_check:
+        print("\nChecking {} . . .\n".format(this_filename))
+        with open(str(PATH) + this_filename, encoding="utf8") as this_file:
             data = this_file.read()
             author_name_list = re.findall('(?:<span class="author--name">)(.*)(?:</span>)', data)
             author_username_list = re.findall('(?:<span class="author--username">)(.*)(?:</span>)', data)
 
         for j in range(len(author_name_list)):
             data_dict = {'name': author_name_list[j], 'username': author_username_list[j]}
-            data_list.append(data_dict)
+            results_list.append(data_dict)
 
-        pp.pprint("File scanned.")
+    print("######################################\n")
+    print("      !! SCANNING COMPLETE !!         \n")
+    print("######################################\n")
+    pp.pprint(results_list)
+    print("\n######################################\n")
 
-    pp.pprint("##############################################")
-    pp.pprint("!! SCANNING COMPLETE !!")
-    pp.pprint("##############################################")
-    pp.pprint(data_list)
-    pp.pprint("##############################################")
+    return results_list
+
+
+def output_results(this_result):
+    print('\nSaving results to {}'.format(OUTPUT_PATH))
+    with open(str(OUTPUT_PATH), 'w+', encoding="utf8") as this_output:
+        this_output.write(str(this_result))
 
 
 if __name__ == '__main__':
     file_list = get_file_list()
-    check_files(file_list)
+    results = check_files(file_list)
+    output_results(results)
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Prooter
+An application to scan through HTML posts to locate data about users and output its findings.
+The intent is to quickly and easily find users, their usernames, and posts they have made.
+Future enhancements will include further investigation of users across the web.
+
+## Contributing
+Created and maintained by Bill Strobl  
+To contribute please create a fork and create a pull request with your changes.
+
+## Release Notes
+Jan 2021 - v0.1 - Basic version created. MVP Only.
